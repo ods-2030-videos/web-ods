e7ff61e579c89ca90912660572e9abfb418e68fb
ods-2030-videos <gp26.adabyron@gmail.com>
2026-04-14 14:39:52 +0200

    Update siteData.js

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -1,16 +1,27 @@
-import supportedLanguages from './content/supportedLanguages.json';
+import configuredLanguages from './content/supportedLanguages.json';
 import odsCatalog from './content/odsCatalog.json';
 import videoSections from './content/videoSections.json';
 import quiz from './content/quiz.json';
 import deepeningSections from './content/deepeningSections.json';
-import esLocale from './content/locales/es.json';
-import enLocale from './content/locales/en.json';
-import euLocale from './content/locales/eu.json';
 
-export { deepeningSections, odsCatalog, quiz, supportedLanguages, videoSections };
+const localeModules = import.meta.glob('./content/locales/*.json', { eager: true });
 
-export const locales = {
-  es: esLocale,
-  en: enLocale,
-  eu: euLocale,
-};
+export const locales = Object.entries(localeModules).reduce((accumulator, [path, module]) => {
+  const match = path.match(/\/([^/]+)\.json$/);
+
+  if (!match) {
+    return accumulator;
+  }
+
+  accumulator[match[1]] = module.default ?? module;
+  return accumulator;
+}, {});
+
+const discoveredLanguages = Object.keys(locales);
+
+export const supportedLanguages = [
+  ...configuredLanguages.filter((lang) => lang in locales),
+  ...discoveredLanguages.filter((lang) => !configuredLanguages.includes(lang)),
+];
+
+export { deepeningSections, odsCatalog, quiz, videoSections };
